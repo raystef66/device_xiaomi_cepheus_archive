d15bce7502b9654687510af4ce8114028e0e9a04
raystef66 <s.vanbarel@gmail.com>
2021-04-01 18:53:21 +0000

    cepheus: test hotword enroll

--- device.mk
+++ device.mk
@@ -229,6 +229,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollment
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
     $(LOCAL_PATH)/configs/permissions/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
