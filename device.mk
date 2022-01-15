3a15ea20392879f25f9fd4366623c8aa7e96b817
¨raystef66¨ <s.vanbarel@gmail.com>
2022-01-15 19:15:58 +0100

    cepheus: crDroid bringup

--- device.mk
+++ device.mk
@@ -361,7 +361,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-lineage
 
 # Power
 PRODUCT_PACKAGES += \
